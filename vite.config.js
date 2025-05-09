6f00d57745deb5bd17cf6475551f28cad53552c0
Anna Kuznetsova <ann_kuzn@mail.ru>
2025-05-09 17:01:04 +0500

    config changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ra16-homeworks-components-1/", // деплой на GitHub Pages
+  base: "/ra16-homeworks-components-2/", // деплой на GitHub Pages
   server: {
     open: true,
   },
